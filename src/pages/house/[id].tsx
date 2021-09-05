f2f8d28b327e9c47d513f4efbdb282bf3e914bff
Ruben de Wit <thesting@rubendewit.nl>
2021-09-05 19:17:15 +0200

    add SSR to House page

--- src/pages/house/[id].tsx
+++ src/pages/house/[id].tsx
@@ -3,6 +3,7 @@ import NextLink from "next/link";
 import { useQuery } from "@apollo/client";
 import { HouseQuery } from "@src/queries";
 import { House } from "@src/types";
+import { initializeApollo } from "@src/utils/apolloClient";
 import { Spinner } from "@src/components/Spinner";
 import { BackLink } from "@src/components/BackLink";
 
@@ -33,7 +34,7 @@ const HousePage: NextPage<HousePageProps> = ({ id }) => {
 export default HousePage;
 
 export const getStaticPaths: GetStaticPaths<HousePageProps> = async () => {
-  // @TODO: Generate static paths from data
+  // TODO: Generate static paths from data
   return {
     paths: [],
     fallback: "blocking",
@@ -47,7 +48,22 @@ export const getStaticProps: GetStaticProps = async (context) => {
     return { notFound: true };
   }
 
-  return {
-    props: { id },
-  };
+  try {
+    const client = initializeApollo();
+    const result = await client.query({ query: HouseQuery, variables: { id } });
+
+    if (!result?.data?.house) {
+      return { notFound: true };
+    }
+
+    return {
+      props: {
+        id,
+        initialApolloState: client.cache.extract(),
+      },
+    };
+  } catch (error) {
+    console.error(`SSR Error occurred while fetching house "${id}"`, error);
+    return { notFound: true };
+  }
 };
