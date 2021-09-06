31cda88058d156cc853cc0cabb47bc267eb9e309
Ruben de Wit <thesting@rubendewit.nl>
2021-09-06 09:35:37 +0200

    use env var for local development

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   reactStrictMode: true,
-  // TODO: Move to .env variable
   publicRuntimeConfig: {
-    graphqlEndpoint: "http://localhost:3000/api/graphql",
+    graphqlEndpoint: process.env.GRAPHQL_ENDPOINT,
   },
 };
